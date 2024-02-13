b4d63f975b91b2e2163590514683174da37ad942
rleusmann <49947587+rleusmann@users.noreply.github.com>
2024-02-13 02:06:34 +0100

    delete unused configuration

--- custom_components/cambridge_audio/__init__.py
+++ custom_components/cambridge_audio/__init__.py
@@ -1,30 +1,14 @@
 """The Cambridge Audio integration."""
 from __future__ import annotations
 
-import voluptuous as vol
-
-from homeassistant.components.media_player import PLATFORM_SCHEMA
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_NAME, Platform
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN, LOGGER
 from .coordinator import CambridgeAudioCoordinator
 
 PLATFORMS = [Platform.MEDIA_PLAYER]
-
-REPO_SCHEMA = vol.Schema(
-    {vol.Required(CONF_HOST): cv.string, vol.Optional(CONF_NAME): cv.string}
-)
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required(CONF_HOST): cv.string,
-        vol.Optional(CONF_NAME, default="CXR200"): cv.string,
-    }
-)
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Cambridge Audio from a config entry."""
